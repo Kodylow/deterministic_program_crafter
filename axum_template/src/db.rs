4d278c3f544e22d2073b77efc46f2d0135408a05
kody.low <kodylow7@gmail.com>
2024-06-07 15:20:39 -0700

    feat: mostly working

---
+++ axum_template/src/db.rs
@@ -0,0 +1,96 @@
+use std::collections::BTreeMap;
+use std::io::Write;
+use std::path::Path;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Task {
+    pub id: u64,
+    pub name: String,
+    pub description: String,
+    pub completed: bool,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct User {
+    pub id: u64,
+    pub username: String,
+    pub password: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Database {
+    pub tasks: BTreeMap<u64, Task>,
+    pub users: BTreeMap<u64, User>,
+}
+
+impl Database {
+    pub fn new() -> Self {
+        Database {
+            tasks: BTreeMap::new(),
+            users: BTreeMap::new(),
+        }
+    }
+
+    pub fn save(&self) -> Result<(), anyhow::Error> {
+        let json = serde_json::to_string(&self)?;
+        let mut file = std::fs::File::create("database.json")?;
+        file.write_all(json.as_bytes())?;
+        Ok(())
+    }
+
+    pub fn load_or_create() -> Result<Self, anyhow::Error> {
+        if !Path::new("database.json").exists() {
+            let database = Database::new();
+            database.save()?;
+        }
+        let json = std::fs::read_to_string("database.json")?;
+        let database: Self = serde_json::from_str(&json)?;
+        Ok(database)
+    }
+
+    pub fn insert_task(&mut self, task: Task) {
+        self.tasks.insert(task.id, task);
+    }
+
+    pub fn update_task(&mut self, task: Task) {
+        self.tasks.insert(task.id, task);
+    }
+
+    pub fn delete_task(&mut self, id: u64) {
+        self.tasks.remove(&id);
+    }
+
+    pub fn get_task(&self, id: u64) -> Option<&Task> {
+        self.tasks.get(&id)
+    }
+
+    pub fn get_tasks(&self) -> Vec<&Task> {
+        self.tasks.values().collect()
+    }
+
+    pub fn insert_user(&mut self, user: User) {
+        self.users.insert(user.id, user);
+    }
+
+    pub fn update_user(&mut self, user: User) {
+        self.users.insert(user.id, user);
+    }
+
+    pub fn delete_user(&mut self, id: u64) {
+        self.users.remove(&id);
+    }
+
+    pub fn get_user(&self, id: u64) -> Option<&User> {
+        self.users.get(&id)
+    }
+
+    pub fn get_user_by_username(&self, username: &str) -> Option<&User> {
+        self.users.values().find(|user| user.username == username)
+    }
+
+    pub fn get_users(&self) -> Vec<&User> {
+        self.users.values().collect()
+    }
+}
--- axum_template/src/main.rs
+++ axum_template/src/main.rs
@@ -1,98 +1,83 @@
-use std::collections::BTreeMap;
-use std::io::Write;
+use std::sync::Arc;
+use std::time::Duration;
 
-use serde::{Deserialize, Serialize};
+use axum::extract::{Path, State};
+use axum::http::header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE};
+use axum::http::HeaderValue;
+use axum::Json;
+use db::{Database, Task};
+use error::AppError;
+use reqwest::StatusCode;
+use tokio::sync::Mutex;
+use tower_http::cors::CorsLayer;
+use tracing::info;
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
-struct Task {
-    id: u64,
-    name: String,
-    description: String,
-    completed: bool,
+mod db;
+mod error;
+
+#[derive(Debug, Clone)]
+struct AppState {
+    db: Arc<Mutex<Database>>,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
-struct User {
-    id: u64,
-    username: String,
-    password: String,
+#[axum::debug_handler]
+async fn create_task(
+    State(app_state): State<AppState>,
+    Json(task): Json<Task>,
+) -> Result<Json<()>, AppError> {
+    let mut db = app_state.db.lock().await;
+    db.insert_task(task);
+
+    // return a 200
+    Ok(Json(()))
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
-struct Database {
-    tasks: BTreeMap<u64, Task>,
-    users: BTreeMap<u64, User>,
-}
-
-impl Database {
-    fn new() -> Self {
-        Database {
-            tasks: BTreeMap::new(),
-            users: BTreeMap::new(),
-        }
-    }
-
-    fn save(&self) -> Result<(), anyhow::Error> {
-        let json = serde_json::to_string(&self)?;
-        let mut file = std::fs::File::create("database.json")?;
-        file.write_all(json.as_bytes())?;
-        Ok(())
-    }
-
-    fn load() -> Result<Self, anyhow::Error> {
-        let json = std::fs::read_to_string("database.json")?;
-        let database: Self = serde_json::from_str(&json)?;
-        Ok(database)
-    }
-
-    fn insert_task(&mut self, task: Task) {
-        self.tasks.insert(task.id, task);
-    }
-
-    fn update_task(&mut self, task: Task) {
-        self.tasks.insert(task.id, task);
-    }
-
-    fn delete_task(&mut self, id: u64) {
-        self.tasks.remove(&id);
-    }
-
-    fn get_task(&self, id: u64) -> Option<&Task> {
-        self.tasks.get(&id)
-    }
-
-    fn get_tasks(&self) -> Vec<&Task> {
-        self.tasks.values().collect()
-    }
-
-    fn insert_user(&mut self, user: User) {
-        self.users.insert(user.id, user);
-    }
-
-    fn update_user(&mut self, user: User) {
-        self.users.insert(user.id, user);
-    }
-
-    fn delete_user(&mut self, id: u64) {
-        self.users.remove(&id);
-    }
-
-    fn get_user(&self, id: u64) -> Option<&User> {
-        self.users.get(&id)
-    }
-
-    fn get_user_by_username(&self, username: &str) -> Option<&User> {
-        self.users.values().find(|user| user.username == username)
-    }
-
-    fn get_users(&self) -> Vec<&User> {
-        self.users.values().collect()
+#[axum::debug_handler]
+async fn read_task(
+    State(app_state): State<AppState>,
+    Path(id): Path<u64>,
+) -> Result<Json<Task>, AppError> {
+    let db = app_state.db.lock().await;
+    match db.get_task(id) {
+        Some(task) => Ok(Json(task.clone())),
+        None => Err(AppError::new(
+            StatusCode::NOT_FOUND,
+            anyhow::anyhow!("Task not found"),
+        )),
     }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
-    println!("Hello, world!");
+    init_logging_and_env()?;
+    let db = Arc::new(Mutex::new(Database::load_or_create()?));
+    let app_state = AppState { db };
+    let cors = CorsLayer::new()
+        .allow_origin(HeaderValue::from_static("http://127.0.0.1:8080"))
+        .allow_methods([axum::http::Method::GET, axum::http::Method::POST])
+        .allow_headers([AUTHORIZATION, CONTENT_TYPE, ACCEPT])
+        .max_age(Duration::from_secs(3600));
 
+    let app = axum::Router::new()
+        .route("/task/:id", axum::routing::get(read_task))
+        .route("/task", axum::routing::post(create_task))
+        .layer(cors)
+        .with_state(app_state);
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8080")
+        .await
+        .map_err(|e| anyhow::anyhow!("Failed to bind to port 8080: {e}"))?;
+    info!("Server listening on http://127.0.0.1:8080");
+
+    axum::serve(listener, app)
+        .await
+        .map_err(|e| anyhow::anyhow!("Failed to start server: {e}"))?;
+
+    Ok(())
+}
+
+fn init_logging_and_env() -> Result<(), anyhow::Error> {
+    tracing_subscriber::fmt::init();
+    dotenv::dotenv().ok();
     Ok(())
 }
